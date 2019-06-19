87e87ae3ec72043613c1d5a9c2e718a6b3ae1814
Katherine Breckenridge <katiebreck@gmail.com>
2019-06-19 13:56:01 -0500

    Started working on theme park Text Adventure

--- Mark8TextAdventure/Program.cs
+++ Mark8TextAdventure/Program.cs
@@ -12,7 +12,38 @@ namespace Mark8TextAdventure
 
             if (response == "yes")
             {
+                Console.WriteLine("Great!Lets go.");
+                Console.WriteLine("Would you like to ride the roller coaster?");
+                response = Console.ReadLine();
 
+                if (response == "yes")
+                {
+                    Console.WriteLine("Okay, we will ride The Fog");
+                    Console.WriteLine("Now that the ride is moving, would you like to stand up");
+
+                    if (response == "yes")
+                    {
+                        Console.WriteLine("Oh, no! Your head fell off!");
+                        Console.WriteLine("Sad day!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Weeeee!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ok, would you like to find another ride?");
+
+                    if (response == "yes")
+                    {
+                        Console.WriteLine("Great, we will walk around until we find something you want to ride.");
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                    }
+                }
             }
 
         }
