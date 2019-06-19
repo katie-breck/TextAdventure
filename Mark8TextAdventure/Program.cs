cd88136a5c4d2482f44a1dc13df85eef769097db
Katherine Breckenridge <katiebreck@gmail.com>
2019-06-19 13:37:11 -0500

    Add project files.

---
+++ Mark8TextAdventure/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Mark8TextAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, welcome to Blahblah Park!");
+            Console.WriteLine("Are you ready to start exploring the park?");
+            string response = Console.ReadLine();
+
+            if (response == "yes")
+            {
+
+            }
+
+        }
+    }
+}
